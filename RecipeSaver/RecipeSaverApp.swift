f788bbb644d579772308238261aa189ea3c3311e
Nikita Shmelev <nikitashmelev7@gmail.com>
2022-01-21 23:23:13 +0700

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by nikita.shmelev on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
