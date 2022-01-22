434e9aa258355118e742cb8964d33ebe684bc37b
Nikita Shmelev <nikitashmelev7@gmail.com>
2022-01-22 17:19:28 +0700

    Add variety of view for recipe

--- RecipeSaver/Views/Main/CategoriesView.swift
+++ RecipeSaver/Views/Main/CategoriesView.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct CategoriesView: View {
-    var body: some View {
-      NavigationView {
-        Text("Categories")
-          .navigationTitle("Categories")
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(Category.allCases) { category in
+          NavigationLink {
+            CategoryView(category: category)
+          } label: {
+            Text(category.rawValue + "s")
+          }
+        }
       }
-      .navigationViewStyle(.stack)
+      .navigationTitle("Categories")
     }
+    .navigationViewStyle(.stack)
+  }
 }
 
 struct CategoriesView_Previews: PreviewProvider {
-    static var previews: some View {
-        CategoriesView()
-    }
+  static var previews: some View {
+    CategoriesView()
+  }
 }
