7c4bdff96dc4ef9e640f1c2ece0489611786ef58
Nikita Shmelev <nikitashmelev7@gmail.com>
2022-02-01 11:50:01 +0700

    Add new view for profile image

--- RecipeSaver/Views/Components/ProfileImageView.swift
+++ RecipeSaver/Views/Components/ProfileImageView.swift
@@ -2,19 +2,47 @@
 //  ProfileImageView.swift
 //  RecipeSaver
 //
-//  Created by nikita.shmelev on 31.01.2022.
+//  Created by nikita.shmelev on 01.02.2022.
 //
 
 import SwiftUI
 
 struct ProfileImageView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @Binding var avatarImage: UIImage
+  @Binding var showingImagePicker: Bool
+  @Binding var isDarkModeOn: Bool
+  
+  var body: some View {
+    HStack {
+      Spacer()
+      
+      Button() {
+        showingImagePicker = true
+      } label: {
+        Image(uiImage: avatarImage)
+          .resizable()
+          .scaledToFill()
+          .padding(avatarImage == UIImage(systemName: "person.fill") ? 30 : 0)
+          .frame(width: 120, height: 120)
+          .background(Color(red: 0.9, green: 0.9, blue: 0.9))
+          .foregroundColor(isDarkModeOn ? Color.white : Color.black)
+          .clipShape(Circle())
+          .clipped()
+          .overlay(Circle().stroke(isDarkModeOn ? Color.white : Color.black, lineWidth: 4))
+          .padding()
+      }
+      
+      Spacer()
     }
+  }
 }
 
 struct ProfileImageView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileImageView()
-    }
+  static var previews: some View {
+    @State var avatarImage = UIImage(systemName: "person.fill")!
+    @State var showingImagePicker = false
+    @State var isDarkModeOn = false
+    
+    return ProfileImageView(avatarImage: $avatarImage, showingImagePicker: $showingImagePicker, isDarkModeOn: $isDarkModeOn)
+  }
 }
--- RecipeSaver/Views/Main/SettingsView.swift
+++ RecipeSaver/Views/Main/SettingsView.swift
@@ -9,43 +9,17 @@ import SwiftUI
 
 struct SettingsView: View {
   @ObservedObject var settingsVM: SettingsViewModel
-  
-  @State private var image = UIImage()
-  
-  @AppStorage("isDarkMode") private var isDarkMode = false
-  
+      
   @State var isAlertShow = false
-  
   @State var isDarkModeOn = false
-  
   @State private var showingImagePicker = false
   @State private var avatarImage = UIImage(systemName: "person.fill")!
   
   var body: some View {
     NavigationView {
       VStack(alignment: .leading) {
-        HStack {
-          Spacer()
-          
-          Button() {
-            showingImagePicker = true
-          } label: {
-            Image(uiImage: avatarImage)
-              .resizable()
-              .scaledToFill()
-              .padding(avatarImage == UIImage(systemName: "person.fill") ? 30 : 0)
-              .frame(width: 120, height: 120)
-              .background(Color(red: 0.9, green: 0.9, blue: 0.9))
-              .foregroundColor(isDarkModeOn ? Color.white : Color.black)
-              .clipShape(Circle())
-              .clipped()
-              .overlay(Circle().stroke(isDarkModeOn ? Color.white : Color.black, lineWidth: 4))
-              .padding()
-          }
-          
-          Spacer()
-        }
-        .padding(.bottom, 20.0)
+        ProfileImageView(avatarImage: $avatarImage, showingImagePicker: $showingImagePicker, isDarkModeOn: $isDarkModeOn)
+          .padding(.bottom, 20.0)
         
         Divider()
         
